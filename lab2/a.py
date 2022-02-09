7c4d34d19ce8a8be94b0c16f7089090a6f66f9c6
Onelvay <98649055+Onelvay@users.noreply.github.com>
2022-02-09 23:36:05 +0600

    Update a.py

--- lab2/a.py
+++ lab2/a.py
@@ -1,9 +1,16 @@
-def check(a):
-    pos = len(a) - 1
-    for i in range(len(a) - 2, -1, -1):
-        if i + a[i] >= pos:
-            pos = i
-    return pos == 0
-
-a=list(map(int, input().split()))
-print( check(a)+0)
+a=input().split()
+list=[]
+for i in range (len(a)):
+    list.append(int(a[i]))
+max_jump=0
+for i in range (len(a)):
+    if max_jump>=i:
+        if list[i]+i>=len(a)-1:
+            print(1)
+            exit()
+        elif list[i]+i>max_jump:
+            max_jump=list[i]+i
+    else:
+        break
+if max_jump<len(a)-1:
+    print(0)
