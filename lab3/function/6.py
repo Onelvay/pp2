aa55949f3454bba01fe325feba0f7d1eace78801
Onelvay <98649055+Onelvay@users.noreply.github.com>
2022-02-17 19:29:51 +0600

    Update 6.py

--- lab3/function/6.py
+++ lab3/function/6.py
@@ -1,5 +1,15 @@
-def reverse(s):
-    return s[::-1]
+def reverse1(s):
+    s=s[::-1]
+    ans=[]
+    hold=''
+    for i in s:
+        if(i==' '):
+            ans.append(hold[::-1])
+            hold=''
+        else:
+            hold+=i
+    ans.append(hold)
+    return ans
 
 x=input()
-print(reverse(x))
+print(*reverse1(x))
