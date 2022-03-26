b61863091845087e94450b2d5687f8a251925915
Onelvay <98649055+Onelvay@users.noreply.github.com>
2022-03-26 20:15:04 +0600

    Create 2.py

---
+++ lab7/2.py
@@ -0,0 +1,46 @@
+import pygame
+
+pygame.init()
+index=0
+memory={
+    0:'acolytewhat3.mp3',
+    1:'arthasyes4.mp3',
+    2:'druidoftheclawready1.mp3',
+    3:'gruntherodies1.mp3',
+    4:'gruntready1.mp3',
+    5:'gryphonriderwhat5.mp3',
+    6:'necromancernogold1.mp3',
+    7:'peasantwhat3.mp3',
+    8:'peonwhat4.mp3',
+    9:'sorceresswhat4.mp3'
+}
+
+screen = pygame.display.set_mode((400, 300))
+done = False
+
+clock = pygame.time.Clock()
+
+
+while not done:
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                        done = True
+                if event.type == pygame.KEYDOWN :
+                    if event.key == pygame.K_RIGHT:
+                        if index<9:
+                            index+=1
+                            pygame.mixer.music.load('C:\\pp2\\lab7\\sounds\\'+memory[index])
+                            pygame.mixer.music.play(0)
+                    if event.key == pygame.K_LEFT:
+                        if index!=0:
+                            index-=1
+                            pygame.mixer.music.load('C:\\pp2\\lab7\\sounds\\'+memory[index])
+                            pygame.mixer.music.play(0)
+                    if event.key == pygame.K_SPACE:
+                        pygame.mixer.music.stop()
+        
+        screen.fill((255, 255, 255)) 
+
+        pygame.display.flip()
+        clock.tick(60)
+pygame.exit()
