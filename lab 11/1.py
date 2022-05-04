ba0160c2ef72395e4568ea7e27b633f46fa95ad1
Onelvay <98649055+Onelvay@users.noreply.github.com>
2022-05-04 20:45:16 +0600

    Update 1.py

--- lab 11/1.py
+++ lab 11/1.py
@@ -1,47 +1,60 @@
 import psycopg2
+import csv
+
+def check_number(phone):
+    phone=str(phone)
+    if phone[:2]=='87' or phone[:2]=='+7':
+        return True
+    else:
+        return False
 
 def limit(cursor):
     print('сколько пользователей вы хотите вывести')
     x=input()
-    cursor.execute('SELECT * FROM contacts LIMIT '+x)
+    cursor.execute('create or replace function limit_(count_ int) RETURNS TABLE( 	name varchar, 	phone int  ) as $$ begin  return query select * from contacts limit count_; end $$ language plpgsql; ')
+    cursor.execute('select * from limit_(%s)',(int(x),))
     records = cursor.fetchall()
     for i in records:
         print(i)
 
 def multiadd(conn,cursor):
-    hold=[]
-    cursor.execute('SELECT * FROM contacts LIMIT 10')
+    cursor.execute('SELECT * FROM contacts')  
     records = cursor.fetchall()
-    not_corr=[]
-    while True:
-        x=input().split()
-        if len(x)<4:
-            not_corr.append(x)
-            break
-        else:
-            hold.append(x)
-    for i in hold:
-        check=True
-        for j in records:
-            if i[1]== j[1]:
+    not_cor=[]
+    with open('C:\\pp2\\11labtest\\sample.txt') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for i in csv_reader:
+            check=True
+            for j in records:
+                if i[1]== j[1]:
+                    check=False
+                    break
+            if not check_number(i[0]):
+                not_cor.append(i)
                 check=False
-                break
-        if check:
-            postgres_insert_query = """ INSERT INTO contacts (user_id,last_name,first_name,phone_number) VALUES (%s,%s,%s,%s)"""
-            record_to_insert = (i[0], i[1], i[2], i[3])
-            cursor.execute(postgres_insert_query, record_to_insert)
-            conn.commit()
-        else:
-            not_corr.append(i)
-    print('неверные данные')
-    print(not_corr)
+            if check:
+                cursor.execute('CREATE or replace FUNCTION new_acc(new_name varchar, new_phone integer) RETURNS void AS   $BODY$       BEGIN         INSERT INTO contacts(name,phone)         VALUES(new_name,new_phone);       END;  $BODY$   LANGUAGE "plpgsql" VOLATILE ')
+                cursor.execute('select new_acc(%s,%s)',(i[0],i[1]))
+                conn.commit()
+            else:
+                cursor.execute('create or replace procedure add_contacts(old_name varchar(255),new_number int ) language plpgsql as $$ begin   update contacts  set phone = new_number   where name=old_name;  end;$$;')
+                cursor.execute('CALL add_contacts(%s,%s)',(i[0],i[1]))
+                conn.commit()
+                # postgres_insert_query = """ INSERT INTO contacts (user_id,last_name,first_name,phone_number) VALUES (%s,%s,%s,%s)"""
+                # record_to_insert = (i[0], i[1], i[2], i[3])
+                # cursor.execute(postgres_insert_query, record_to_insert)
+                # conn.commit()
+    if not_cor:
+        print('неверные данные')
+        print(not_cor)
+
     return True
 
 
 def pattern(cursor):
     print('введите начало шаблона имени')
     y=input()
-    cursor.execute('SELECT * FROM contacts LIMIT 10')
+    cursor.execute('SELECT * FROM contacts')
     print('все совпадения')
     records=cursor.fetchall()
     for i in records:
@@ -54,19 +67,19 @@ def delete(conn,cursor):
     print('удаление по номеру 1, по имени 2')
     x=input()
     if x == '1':
-        x=input()
-        sql_delete_query = """Delete from contacts where phone_number= %s"""
-        cursor.execute(sql_delete_query, (x,))
+        x=int(input())
+        cursor.execute('create or replace procedure delete_(   old_name integer ) language plpgsql as $$ begin   delete from contacts  where phone=old_name; end;$$; ')
+        cursor.execute('call delete_(%s)',(x,))
         conn.commit()
     else:
         x=input()
-        sql_delete_query = """Delete from contacts where last_name= %s"""
-        cursor.execute(sql_delete_query, (x,))
+        cursor.execute('create or replace procedure delete_(   old_name varchar(255) ) language plpgsql as $$ begin   delete from contacts  where name=old_name; end;$$; ')
+        cursor.execute('call delete_(%s)',(x,))
         conn.commit()
     print("Удаление произошло успешно")
 
 def update(conn,cursor):
-    print('Введите ID контакта которог хотите обновить')
+    print('Введите ID контакта которогo хотите обновить')
     x=int(input())
 
     print('что вы хотите обновить?')
@@ -82,7 +95,7 @@ def update(conn,cursor):
         cursor.execute(sql_update_query, (z, x))
         conn.commit()
 
-    elif y=='фамилие':
+    elif y=='фамилия':
         sql_update_query = """Update contacts set last_name = %s where user_id = %s"""
         cursor.execute(sql_update_query, (z, x))
         conn.commit()
@@ -95,80 +108,60 @@ def update(conn,cursor):
     print('УСПЕШНО!')
 
 def show(cursor):
-    print('Напишите что вы хотите вывести')
-    print('1:ID')
-    print('2:first_name')
-    print('3:all')
-    print('4: phone numbers')
-    cursor.execute('SELECT * FROM contacts LIMIT 10')
-    records = cursor.fetchall()
-    k=int(input())
-    if k == 1:
-        for row in records:
-            print(row[0])
-    elif k == 2:
-        for row in records:
-            print(row[1])
-    elif k==3:
-        for row in records:
-            print(row)
-    else:
-        for row in records:
-            print(row[3])
-
+    print('только номера')
+    cursor.execute('create or replace function onlynumbers() RETURNS TABLE(	phone int ) as $$ begin  return query select contacts.phone from contacts; end $$ language plpgsql;  select *from onlynumbers();')
+    show=cursor.fetchall()
+    print(show)
+    print('имя и номер')
+    cursor.execute('create or replace function ALL_() RETURNS TABLE( 	name varchar, 	phone int ) as $$ begin  return query select * from contacts; end $$ language plpgsql;  select *from ALL_();')
+    show=cursor.fetchall()
+    print(show)
 
 def add(conn,cursor):
-    print('Введите ID,имя, фамилие и номер пользователя')
+    print('имя и номер пользователя')
     z=input().split()
-    check=True
-    cursor.execute('SELECT * FROM contacts LIMIT 10')
-    records = cursor.fetchall()
-    for i in records:
-        print(z[1],i[1])
-        if z[1]==i[1]:
-            sql_update_query = """Update contacts set user_id = %s,first_name=%s,phone_number=%s where last_name = %s"""
-            cursor.execute(sql_update_query, (z[0],z[2],z[3],z[1] ))
-            conn.commit()
-            check=False
-            break
-    if check:
-        postgres_insert_query = """ INSERT INTO contacts (user_id,last_name,first_name,phone_number) VALUES (%s,%s,%s,%s)"""
-        record_to_insert = (z[0], z[1], z[2], z[3])
-        cursor.execute(postgres_insert_query, record_to_insert)
+    old_name=z[0]
+    new_number=int(z[1])
+    cursor.execute('select phone from contacts where name=%s',(old_name,))
+    show=cursor.fetchall()
+    if show:
+        cursor.execute('create or replace procedure add_contacts(old_name varchar(255),new_number int ) language plpgsql as $$ begin   update contacts  set phone = new_number   where name=old_name;  end;$$;')
+        cursor.execute('CALL add_contacts(%s,%s)',(old_name,new_number))
+        conn.commit()
+    else:
+        cursor.execute('CREATE or replace FUNCTION new_acc(new_name varchar, new_phone integer) RETURNS void AS   $BODY$       BEGIN         INSERT INTO contacts(name,phone)         VALUES(new_name,new_phone);       END;  $BODY$   LANGUAGE "plpgsql" VOLATILE ')
+        cursor.execute('select new_acc(%s,%s)',(old_name,new_number))
         conn.commit()
 
     print( "УСПЕШНО!")
 
-try:
-    conn = psycopg2.connect("dbname=postgres user=postgres password=Adg12332,")
-    cursor = conn.cursor()
-    print('Выберите что вы хотите сделать:')
-    print('1:Добавить контакт')
-    print('2:Удалить контакт')
-    print('3:Показать контакты')
-    print('4:Обновить контакт')
-    print('5:Шаблон')
-    print('6:mutliadd')
-    print('7:limit')
-    choose=input()
-    if choose == '2':
-        delete(conn,cursor)
-    elif choose == '1':
-        add(conn,cursor)
-    elif choose=='3':
-        show(cursor)
-    elif choose=='4':
-        update(conn,cursor)
-    elif choose == '5':
-        pattern(cursor)
-    elif choose=='6':
-        multiadd(conn,cursor)
-    elif choose=='7':
-        limit(cursor)
 
-except psycopg2.Error as e:
-    print('fail')
+conn = psycopg2.connect("dbname=lab11 user=postgres password=Adg12332,")
+cursor = conn.cursor()
+print('Выберите что вы хотите сделать:')
+print('1:Добавить контакт')
+print('2:Удалить контакт')
+print('3:Показать контакты')
+print('4:Обновить контакт')
+print('5:Шаблон')
+print('6:mutliadd')
+print('7:limit')
+print('8:create table')
+choose=input()
+if choose == '2':
+    delete(conn,cursor)
+elif choose == '1':
+    add(conn,cursor)
+elif choose=='3':
+    show(cursor)
+elif choose=='4':
+    update(conn,cursor)
+elif choose == '5':
+    pattern(cursor)
+elif choose=='6':
+    multiadd(conn,cursor)
+elif choose=='7':
+    limit(cursor)
 
-finally: 
-    cursor.close()
-    conn.close()
+
+
